f5c7a399fea143bfa09513f1d90083d6ad17dda3
Jerin Prakash <jerinprakash892@gmail.com>
2026-01-11 10:48:31 +0530

    Updated the code

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -1,5 +1,19 @@
 import java.util.Scanner;
 
 public class PatternPrinting {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       // System.out.print("Enter n : ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            System.out.print("* ");
+        }
+        System.out.println();
+        for (int i = 1; i <= n; i++) {
+            System.out.println("*");
+        }
+    }
 }
